88f59e5f8009071da1014098ee718401aa5e5f2f
Corinne Bailly <web.bacor@gmail.com>
2018-08-21 16:38:03 +0200

    problème BD

--- config/config.php
+++ config/config.php
@@ -1,8 +1,41 @@
 <?php
+
+
 // établir la connection à la base de données
-return array(
+/*return array(
     "db_user" => "root",    
     "db_pass" => "",
     "db_host" => "localhost",
     "db_name" => "alaska"
+);*/
+return array(
+    "db_user" => "root",    
+    "db_pass" => "",
+    "db_host" => "localhost",//https://phpmyadmin.1and1.fr/index.php?token=25cf085711e22d5ddf424f3a4ded9cb5",
+    "db_name" => "alaska"//db750395619"
 );
+/*
+$host_name = 'db750395619.db.1and1.com';
+$database = 'db750395619';
+$user_name = 'dbo750395619';
+$password = 'AlaskaP4*';
+$connect = mysqli_connect($host_name, $user_name, $password, $database);
+
+if (mysqli_connect_errno()) {
+    die('<p>La connexion au serveur MySQL a échoué: '.mysqli_connect_error().'</p>');
+} else {
+    echo '<p>Connexion au serveur MySQL établie avec succès.</p >';
+}*/
+/*
+$host_name = 'db750395619.db.1and1.com';
+$database = 'db750395619';
+$user_name = 'alaska';
+$password = 'Alaskap4*';
+$connect = mysqli_connect($host_name, $user_name, $password, $database);
+
+if (mysqli_connect_errno()) {
+    die('<p>La connexion au serveur MySQL a échoué: '.mysqli_connect_error().'</p>');
+} else {
+    echo '<p>Connexion au serveur MySQL établie avec succès.</p >';
+}*/
+?>
