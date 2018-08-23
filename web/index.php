9caf5523a79cc12c2533269c4afc65e50ea9fb97
Corinne Bailly <web.bacor@gmail.com>
2018-08-23 14:45:14 +0200

    rectification index.php retirer head

--- web/index.php
+++ web/index.php
@@ -1,44 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-       
-    
-  <meta charset="utf-8">
-  <meta name="description" content="billet simple pour l'Alaska" />
- 
-  <title>J Forteroche</title>
-    <link rel="stylesheet" type="text/css" href="web/css/style.css"/>
-    
-<!--favicon-->
-      
-  <link rel="shortcut icon" href="img/favicon.png" />
-      
-<!-- régler la mise en page par rapport aux tailles/résolutions d'écrans -->
-  <meta name="viewport" content="width=device-width, initial-scale=1"/>  
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-       
-<!-- pour optimiser le réferencement-->
-      <meta name="keywords" content="billet simple pour l'Alaska, Jean Forteroche" />
-      <meta name="author" content="Corinne" />
-    
- <!-- OpenGraph (pour optimiser le réferencement)FACEBOOK-->
-    <meta property="og:title" content="billet simple pour Alaska Jean Forteroche"/>
-    <meta property="og:type" content="website"/>
-    <meta property="og:description" content="billet simple pour Alaska Jean Forteroche, ecrivain, blog, commentaires" />
-    <meta property="og:url" content=""/>
-    <meta property="og:image" content=""/>
-   
- <!-- OpenGraph (pour optimiser le réferencement)TWITTER  -->
-      <meta name=”twitter:card” content="summary" />
-      <meta name=”twitter:site” content="" />
-      <meta name=”twitter:title” content="billet simple pour Alaska Jean Forteroche" />
-      <meta name=”twitter:description” content="billet simple pour Alaska Jean Forteroche, ecrivain, blog, commentaires"  />
-      <meta name=”twitter:image” content="" />
-       
-</head>
-<body>
-  <?php
+<?php
 
 // définit le chemin racine de l'application
 define('ROOT', dirname(__DIR__));
@@ -70,12 +30,9 @@ $page = explode('.', $page); //retourne le tableau des chaines
        $action = $page[1];
     }
 
+
 // création et action du contrôleur nécessaire.
 $controller = new $controller();
 $controller->$action();
 
 ?>
-
-
-</body>
-</html>
