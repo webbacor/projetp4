50e3adfedcc32f74cd4579bd0ac1288ec2c62401
Corinne Bailly <web.bacor@gmail.com>
2018-08-23 15:00:37 +0200

    separation pages head et footer

---
+++ app/Views/templates/htmlfooter.php
@@ -0,0 +1,37 @@
+<footer>
+       
+    <div class="ecrivain">
+       <img src="img/ecrivain.jpg" alt="ecrivain" >
+
+       <div histoire> <p>
+
+             Mon Roman en ligne<br/>
+
+             Bienvenue sur le site de mon roman en ligne, c'est un nouveau concept que je mets en pratique, je posterai à intervales réguliers les épisodes de mon nouveau roman intitulé "Billet simple pour l'Alaska".<br/>
+
+             J'espère que ce concept et son contenu vous plaîront, n'hésitez pas à laisser vos avis et réactions en commentaires,vous pouvez m'envoyer également un message via <a href=mailto:web.bacor@gmail>JForteroche@gmail.com</a>
+             <br/>Bonne lecture et merci de l'interêt porté à mon roman.<br/></p>
+            <br/>
+
+        </div>
+  
+    </div>
+
+    <img src="img/signature.png" alt="signature"/>
+       
+  <br/>
+ <img src="img/livreban.png" alt="livre footer"><br/>
+<button data-toggle="modal" href="#infos" class="btn btn-primary">Mentions Légales</button>
+
+        <?php 
+        require '../web/mentions.php';
+        ?>
+
+<button data-toggle="modal" href="#infop" class="btn btn-primary">Politique de confidentilité</button>
+
+      <?php 
+        require '../web/pconfid.php';
+        ?>
+        
+ <br/>
+</footer>
---
+++ app/Views/templates/htmlhead.php
@@ -0,0 +1,65 @@
+<head>
+
+     <meta charset="utf-8 without bom">
+     <meta name="description" content="billet simple pour l'Alaska" />
+    <title>J Forteroche</title>
+  
+
+    
+    
+    <link rel="shortcut icon" href="img/favicon.png" />
+    <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/alaska.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="css/alaska.css"/>
+    
+   
+
+  <!-- régler la mise en page par rapport aux tailles/résolutions d'écrans -->
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>  
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+       
+<!-- pour optimiser le réferencement-->
+    <meta name="keywords" content="billet simple pour l'Alaska, Jean Forteroche" />
+    <meta name="author" content="Jean Forteroche" />
+    
+ <!-- OpenGraph (pour optimiser le réferencement)FACEBOOK-->
+    <meta property="og:title" content="billet simple pour Alaska Jean Forteroche"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:description" content="billet simple pour Alaska Jean Forteroche, ecrivain, blog, commentaires" />
+    <meta property="og:url" content=""/>
+    <meta property="og:image" content=""/>
+   
+ <!-- OpenGraph (pour optimiser le réferencement)TWITTER  -->
+      <meta name=”twitter:card” content="summary" />
+      <meta name=”twitter:site” content="" />
+      <meta name=”twitter:title” content="billet simple pour Alaska Jean Forteroche" />
+      <meta name=”twitter:description” content="billet simple pour Alaska Jean Forteroche, ecrivain, blog, commentaires"  />
+      <meta name=”twitter:image” content="" />
+
+    <script src='lib/tinymce/tinymce.min.js'></script>
+    
+
+    <script>
+        tinymce.init({
+            selector: '#tinytextarea',
+            language: 'fr_FR',
+            height: 500,
+            menubar: false,
+            plugins: [
+                'advlist autolink lists link image charmap print preview anchor',
+                'searchreplace visualblocks code fullscreen',
+                'insertdatetime media table contextmenu paste code textcolor colorpicker '
+            ],
+            toolbar: 'undo redo | insert | styleselect | bold italic | forecolor  | alignleft aligncenter alignright alignjustify' +
+            ' | bullist numlist outdent indent | link image'
+        });
+    </script>
+
+    
+ 
+</head>
+
+
+
+ 
