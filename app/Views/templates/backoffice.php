d8214264db325cbb83326ee9047f23e0e7e521e6
Corinne Bailly <web.bacor@gmail.com>
2018-08-23 14:59:13 +0200

    separation head et footer des pages backoffice et default

--- app/Views/templates/backoffice.php
+++ app/Views/templates/backoffice.php
@@ -1,40 +1,10 @@
 <!doctype html>
 <html>
-<head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">  
-    <meta name="description" content="">
-    <meta name="author" content="Jean Forteroche">
-    <link rel="shortcut icon" href="img/favicon.png" />
-    <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="css/alaska.css" rel="stylesheet" />
+<?php
+    require ('htmlhead.php');
+?>
 
-    
-
-    <script src='lib/tinymce/tinymce.min.js'></script>
-    
-
-    <script>
-        tinymce.init({
-            selector: '#tinytextarea',
-            language: 'fr_FR',
-            height: 500,
-            menubar: false,
-            plugins: [
-                'advlist autolink lists link image charmap print preview anchor',
-                'searchreplace visualblocks code fullscreen',
-                'insertdatetime media table contextmenu paste code textcolor colorpicker '
-            ],
-            toolbar: 'undo redo | insert | styleselect | bold italic | forecolor  | alignleft aligncenter alignright alignjustify' +
-            ' | bullist numlist outdent indent | link image'
-        });
-    </script>
-
-    <title>billet simple pour l'Alaska</title>
- 
-</head>
     
 <body>
     
