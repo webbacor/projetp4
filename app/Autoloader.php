a1f4cb6c53f48ad82618316e2f0218f73fb7e88c
Corinne Bailly <web.bacor@gmail.com>
2018-08-23 14:57:04 +0200

    rectification autoloader verif sur serveur

--- app/Autoloader.php
+++ app/Autoloader.php
@@ -16,9 +16,8 @@ class Autoloader{
         if (strpos($class, __NAMESPACE__ . '\\') === 0){
             $class = str_replace(__NAMESPACE__ . '\\', '', $class);
             $class = str_replace('\\', '/', $class);
+           
             require __DIR__ . '/' . $class . '.php';
-           
-           
         }
     }
 
